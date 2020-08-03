c00daaa5c20d5300178414027972b070efa389cf
Andrew Piper <andrew@threax.com>
2020-08-03 13:55:18 -0400

    Moved connecting the ssh key. Also disconnect it when done.

--- ExampleDeployment/RemoteSetupAll.ps1
+++ ExampleDeployment/RemoteSetupAll.ps1
@@ -1,13 +1,19 @@
 param ([Parameter(Position=0,mandatory=$true)]$sshConnection)
 
+# Connect ssh key
+Get-Content ~\.ssh\id_rsa.pub | ssh $sshConnection "umask 077; test -d .ssh || mkdir .ssh ; cat > .ssh/authorized_keys || exit 1"
+
 $scriptPath = Split-Path $script:MyInvocation.MyCommand.Path
 &"$scriptPath/init/Remote.ps1" $sshConnection
 &"$scriptPath/nginx/Remote.ps1" $sshConnection
 &"$scriptPath/id/Remote.ps1" $sshConnection
 &"$scriptPath/appdashboard/Remote.ps1" $sshConnection
-'*************************************************'
-'Remote Server Setup Complete'
-'Visit https://id.dev.threax.com/Manage/Index and create an account.'
+Write-Host '*************************************************'
+Write-Host 'Remote Server Setup Complete'
+Write-Host 'Visit https://id.dev.threax.com/Manage/Index and create an account.'
 $userId = Read-Host -Prompt 'Enter the new user''s User Id here'
 ssh -t $sshConnection "sudo Threax.DockerTools exec /app/id/appsettings.json AddAdmin $userId"
-'Visit https://appdashboard.dev.threax.com to test the account.'
+Write-Host 'Visit https://appdashboard.dev.threax.com to test the account.'
+
+# Disconnect ssh key
+ssh $sshConnection "umask 077; rm .ssh/authorized_keys || exit 1"
--- ExampleDeployment/init/Remote.ps1
+++ ExampleDeployment/init/Remote.ps1
@@ -1,8 +1,5 @@
 param ([Parameter(Position=0,mandatory=$true)]$sshConnection)
 
-# Connect ssh key
-Get-Content ~\.ssh\id_rsa.pub | ssh $sshConnection "umask 077; test -d .ssh || mkdir .ssh ; cat > .ssh/authorized_keys || exit 1"
-
 # Initialize
 $scriptPath = Split-Path $script:MyInvocation.MyCommand.Path
 $dirName = [System.IO.Path]::GetFileName($scriptPath)
