e157a1559407d6ff3562744e76c7a5be9529517e
Andrew Piper <andrew@threax.com>
2020-08-04 18:03:12 -0400

    Changed docker build to be a build with docker.

--- Threax.DockerTools/DockerfileBuild.ps1
+++ Threax.DockerTools/DockerfileBuild.ps1
@@ -1,3 +1,4 @@
 $scriptPath = Split-Path $script:MyInvocation.MyCommand.Path
 
-docker build "$scriptPath/.." -f "$scriptPath/Dockerfile" -t threax/docker-tools
+docker build "$scriptPath/.." -f "$scriptPath/Dockerfile" -t threax-docker-tools-builder # This builds the tools with docker
+docker run -it --rm -v "${pwd}/bin:/out" threax-docker-tools-builder # This run line extracts the compiled tools from the image
