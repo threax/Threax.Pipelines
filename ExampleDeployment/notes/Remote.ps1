c3361dba78193af8d00770e20efbb3744e995945
Andrew Piper <andrew@threax.com>
2020-08-04 11:20:05 -0400

    Set secret names to work with keys from files.

--- ExampleDeployment/notes/Remote.ps1
+++ ExampleDeployment/notes/Remote.ps1
@@ -13,13 +13,15 @@ ssh -t $sshConnection "sudo cp -r ~/$dirName /app;rm -r ~/$dirName"
 ssh -t $sshConnection "sudo Threax.DockerTools clone /app/$dirName/appsettings.json"
 ssh -t $sshConnection "sudo Threax.DockerTools build /app/$dirName/appsettings.json"
 
-$secretName = "client-secret"
+$clientSecretName = "JwtAuth__ClientSecret";
+$secretName = $clientSecretName
 ssh $sshConnection "if test -f /app/$dirname/secrets/$secretName; then exit 0; else exit 1; fi"
 if($LASTEXITCODE -eq 1) {
     ssh -t $sshConnection "openssl rand -base64 32 > ~/$secretName; sudo Threax.DockerTools setsecret /app/$dirName/appsettings.json $secretName ~/$secretName; rm ~/$secretName"
 }
 
-$secretName = "client-creds-secret"
+$clientCredsSecretName = "SharedClientCredentials__ClientSecret"
+$secretName = $clientCredsSecretName
 ssh $sshConnection "if test -f /app/$dirname/secrets/$secretName; then exit 0; else exit 1; fi"
 if($LASTEXITCODE -eq 1) {
     ssh -t $sshConnection "openssl rand -base64 32 > ~/$secretName; sudo Threax.DockerTools setsecret /app/$dirName/appsettings.json $secretName ~/$secretName; rm ~/$secretName"
@@ -30,10 +32,10 @@ ssh -t $sshConnection "sudo Threax.DockerTools run /app/$dirName/appsettings.jso
 
 # Load app metadata into id server
 ssh -t $sshConnection "sudo mkdir -p /app/id/data/load/$dirName"
-ssh -t $sshConnection "sudo cp /app/$dirname/secrets/client-secret /app/id/data/load/$dirName/client-secret"
-ssh -t $sshConnection "sudo cp /app/$dirname/secrets/client-creds-secret /app/id/data/load/$dirName/client-creds-secret"
+ssh -t $sshConnection "sudo cp /app/$dirname/secrets/$clientSecretName /app/id/data/load/$dirName/$clientSecretName"
+ssh -t $sshConnection "sudo cp /app/$dirname/secrets/$clientCredsSecretName /app/id/data/load/$dirName/$clientCredsSecretName"
 ssh -t $sshConnection "sudo chown -R 19999:19999 /app/id/data/load/$dirName"
 
-ssh -t $sshConnection "sudo Threax.DockerTools exec /app/id/appsettings.json AddFromMetadata https://notes.dev.threax.com /load/$dirName/client-secret /load/$dirName/client-creds-secret"
+ssh -t $sshConnection "sudo Threax.DockerTools exec /app/id/appsettings.json AddFromMetadata https://notes.dev.threax.com /load/$dirName/$clientSecretName /load/$dirName/$clientCredsSecretName"
 
 ssh -t $sshConnection "sudo rm -r /app/id/data/load/$dirName"
