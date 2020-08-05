b775cc172a07797ff4cdfe5765db766caed0bb5f
Andrew Piper <andrew@threax.com>
2020-08-05 14:26:00 -0400

    Changed publish to build.

---
+++ Threax.DockerTools/Build.ps1
@@ -0,0 +1,21 @@
+param (
+    [Parameter(Position=0,mandatory=$true)]$destDir,
+    [Parameter(Position=1)]$unixTarget = "ubuntu.18.04-x64"
+)
+
+$scriptPath = Split-Path $script:MyInvocation.MyCommand.Path
+
+$os = [System.Environment]::OSVersion.Platform
+
+if($os -eq [System.PlatformID]::Unix){
+    $target = $unixTarget
+}
+else {
+    $target = "win-x64"
+}
+
+Remove-Item -Recurse "$destDir/bin" -ErrorAction 'SilentlyContinue' # Have to call this every time or errors can occur
+
+# Build the image then extract the tool from it by running it.
+docker build --build-arg TARGET=$target "$scriptPath/.." -f "$scriptPath/Dockerfile" -t threax-docker-tools-builder
+docker run -it --rm -v "${destDir}:/out" threax-docker-tools-builder
--- Threax.DockerTools/Publish.ps1
+++
@@ -1,2 +0,0 @@
-dotnet publish -r ubuntu.18.04-x64 -c Release /p:PublishSingleFile=true /p:PublishTrimmed=true
-dotnet publish -c Release
