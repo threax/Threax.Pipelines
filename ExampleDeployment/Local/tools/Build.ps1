a7ebcf970cde4cc66412f833d72531cee5b30aa4
Andrew Piper <andrew@threax.com>
2020-08-05 10:03:42 -0400

    Clone pipeline repo to build tools.

--- ExampleDeployment/Local/tools/Build.ps1
+++ ExampleDeployment/Local/tools/Build.ps1
@@ -1,6 +1,19 @@
 $scriptPath = Split-Path $script:MyInvocation.MyCommand.Path
 
-docker build "$scriptPath/../../../" -f "$scriptPath/../../../Threax.DockerTools/Dockerfile" -t threax-docker-tools-builder # This builds the tools with docker
+$toolsRepo = "https://github.com/threax/Threax.Pipelines.git"
+$srcDir = "$scriptPath/src"
+if(Test-Path $srcDir) {
+    Push-Location $srcDir
+    git pull
+    Pop-Location
+}
+else {
+    git clone $toolsRepo $srcDir
+}
+
+Remove-Item -Recurse "$scriptPath/ubuntu"
+Remove-Item -Recurse "$scriptPath/win"
+docker build "$srcDir" -f "$srcDir/Threax.DockerTools/Dockerfile" -t threax-docker-tools-builder # This builds the tools with docker
 docker run -it --rm -v "${scriptpath}:/out" threax-docker-tools-builder # This run line extracts the compiled tools from the image
 Remove-Item -Recurse "$scriptPath/bin"
 Move-Item "$scriptPath/win" "$scriptPath/bin"
