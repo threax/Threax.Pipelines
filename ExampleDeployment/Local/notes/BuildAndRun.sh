0d62b4078d901562512d9717add47c0aff9773cf
Andrew Piper <andrew@threax.com>
2020-08-06 17:53:59 -0400

    Added buld and run script to notes.

---
+++ ExampleDeployment/Local/notes/BuildAndRun.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Thanks to https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself?page=1&tab=votes#tab-top
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+scriptPath="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+$scriptPath/../tools/bin/Threax.DockerTools clone $scriptPath/appsettings.json
+$scriptPath/../tools/bin/Threax.DockerTools build $scriptPath/appsettings.json
+$scriptPath/../tools/bin/Threax.DockerTools run $scriptPath/appsettings.json
