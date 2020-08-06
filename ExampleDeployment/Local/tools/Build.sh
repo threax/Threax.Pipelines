ac9018246b9924a6af3ef3d30fae1ae60d7b4418
Andrew Piper <andrew@threax.com>
2020-08-06 16:23:59 -0400

    Adding some bash build scripts.

---
+++ ExampleDeployment/Local/tools/Build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Thanks to https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself?page=1&tab=votes#tab-top
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+scriptPath="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+toolsRepo = "https://github.com/threax/Threax.Pipelines.git"
+srcDir = "$scriptPath/src/Threax.Pipelines"
+if [ -d $srcDir ]
+then
+    pushd $srcDir
+    git pull
+    popd
+else
+    git clone $toolsRepo $srcDir
+fi
+
+bash -c $srcDir/Threax.DockerTools/Build.sh "linux-arm64"
---
+++ Threax.DockerTools/Build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Thanks to https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself?page=1&tab=votes#tab-top
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+scriptPath="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+target=$0
+
+docker build --build-arg TARGET=$target "$scriptPath/.." -f "$scriptPath/Dockerfile" -t threax-docker-tools-builder
+docker run -it --rm -v "${destDir}:/out" threax-docker-tools-builder
