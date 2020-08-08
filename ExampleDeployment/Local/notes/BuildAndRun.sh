d24d29e2bace79b060e560f4069366df0f761fee
Andrew Piper <andrew@threax.com>
2020-08-08 10:24:47 -0400

    Fixed SetupAppDashboard to AddFromMetadata.

--- ExampleDeployment/Local/notes/BuildAndRun.sh
+++ ExampleDeployment/Local/notes/BuildAndRun.sh
@@ -13,6 +13,6 @@ dirName="$(basename $scriptPath)"
 $scriptPath/../tools/bin/Threax.DockerTools clone $scriptPath/appsettings.json
 $scriptPath/../tools/bin/Threax.DockerTools build $scriptPath/appsettings.json
 $scriptPath/../tools/bin/Threax.DockerTools run $scriptPath/appsettings.json
-$scriptPath/../tools/bin/Threax.DockerTools exec $scriptPath/../id/appsettings.json SetupAppDashboard \
+$scriptPath/../tools/bin/Threax.DockerTools exec $scriptPath/../id/appsettings.json AddFromMetadata \
     -l secret $dirName/JwtAuth__ClientSecret $scriptPath/appsettings.json JwtAuth__ClientSecret
     -l secret $dirName/SharedClientCredentials__ClientSecret $scriptPath/appsettings.json SharedClientCredentials__ClientSecret
--- ExampleDeployment/Local/notes/Local.ps1
+++ ExampleDeployment/Local/notes/Local.ps1
@@ -4,6 +4,6 @@ $dirName = [System.IO.Path]::GetFileName($scriptPath)
 &"$scriptPath/../tools/bin/Threax.DockerTools" clone $scriptPath/appsettings.json
 &"$scriptPath/../tools/bin/Threax.DockerTools" build $scriptPath/appsettings.json; if($LASTEXITCODE -ne 0) {throw "Error during build."}
 &"$scriptPath/../tools/bin/Threax.DockerTools" run $scriptPath/appsettings.json
-&"$scriptPath/../tools/bin/Threax.DockerTools" exec $scriptPath/../id/appsettings.json SetupAppDashboard `
-    -l secret $dirName/JwtAuth__ClientSecret $scriptPath/appsettings.json JwtAuth__ClientSecret
+&"$scriptPath/../tools/bin/Threax.DockerTools" exec $scriptPath/../id/appsettings.json AddFromMetadata `
+    -l secret $dirName/JwtAuth__ClientSecret $scriptPath/appsettings.json JwtAuth__ClientSecret `
     -l secret $dirName/SharedClientCredentials__ClientSecret $scriptPath/appsettings.json SharedClientCredentials__ClientSecret
