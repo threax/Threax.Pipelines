85b0b453fc8c356479c6612f16c12ca4568348a1
Andrew Piper <andrew@threax.com>
2020-08-10 18:26:15 -0400

    Moved urls inside config files.

--- ExampleDeployment/Local/notes/BuildAndRun.sh
+++ ExampleDeployment/Local/notes/BuildAndRun.sh
@@ -14,6 +14,6 @@ $scriptPath/../tools/bin/Threax.DockerTools clone $scriptPath/appsettings.json
 $scriptPath/../tools/bin/Threax.DockerTools build $scriptPath/appsettings.json
 $scriptPath/../tools/bin/Threax.DockerTools run $scriptPath/appsettings.json
 $scriptPath/../tools/bin/Threax.DockerTools exec $scriptPath/../id/appsettings.json AddFromMetadata \
-    "https://$dirName.dev.threax.com" \
+    "$dirName" \
     --exec-load secret $dirName/JwtAuth__ClientSecret $scriptPath/appsettings.json JwtAuth__ClientSecret \
     --exec-load secret $dirName/SharedClientCredentials__ClientSecret $scriptPath/appsettings.json SharedClientCredentials__ClientSecret
--- ExampleDeployment/Local/notes/Local.ps1
+++ ExampleDeployment/Local/notes/Local.ps1
@@ -5,6 +5,6 @@ $dirName = [System.IO.Path]::GetFileName($scriptPath)
 &"$scriptPath/../tools/bin/Threax.DockerTools" build $scriptPath/appsettings.json; if($LASTEXITCODE -ne 0) {throw "Error during build."}
 &"$scriptPath/../tools/bin/Threax.DockerTools" run $scriptPath/appsettings.json
 &"$scriptPath/../tools/bin/Threax.DockerTools" exec $scriptPath/../id/appsettings.json AddFromMetadata `
-    "https://$dirName.dev.threax.com" `
+    "$dirName" `
     --exec-load secret "$dirName/JwtAuth__ClientSecret" "$scriptPath/appsettings.json" "JwtAuth__ClientSecret" `
     --exec-load secret "$dirName/SharedClientCredentials__ClientSecret" "$scriptPath/appsettings.json" "SharedClientCredentials__ClientSecret"
