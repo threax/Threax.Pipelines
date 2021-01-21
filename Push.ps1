3c0c21c23bbd22717f3cdff5f70529fa805db9db
Andrew Piper <andrew@threax.com>
2021-01-21 10:04:03 -0500

    Added push script.

---
+++ Push.ps1
@@ -0,0 +1,4 @@
+git push --all
+git push https://github.com/threax/Threax.Pipelines.git --all
+git push https://gitlab.com/Threax/threax.pipelines.git --all
+git push https://threax@dev.azure.com/threax/ThreaxSoftware/_git/Threax.Pipelines --all
